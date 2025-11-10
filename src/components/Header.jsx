67c739abc972305e549781c6b33d9a1fc1081529
codephrem <abemqal4@gmail.com>
2025-11-10 09:49:14 +0300

    Remove Header component implementation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,1 @@
-import React from "react";
-import "../styles/Header.css";
 
-function Header() {
-	return (
-		<header className="header">
-			<div className="logo">SmartAgriTrade</div>
-			<nav className="nav-links">
-				<a href="#home">Home</a>
-				<a href="#services">Services</a>
-				<a href="#predict">Predict</a>
-				<a href="#about">About Us</a>
-				<a href="#signup">Sign Up</a>
-			</nav>
-		</header>
-	);
-}
-
-export default Header;
